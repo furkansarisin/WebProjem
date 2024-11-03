24dec1a6a479dcae614f7c22b084ac99d0c9ce8b
Furkan Sarışın <92827283+furkansarisin@users.noreply.github.com>
2024-11-03 14:14:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projectsSection = document.getElementById('projects');
-    projectsSection.style.opacity = 0;
-
-    setTimeout(() => {
-        projectsSection.style.transition = 'opacity 1s';
-        projectsSection.style.opacity = 1;
-    }, 100);
-});
+// Burada daha sonra etkileşimli özellikler ekleyebilirsiniz
+console.log("Web sitesi yüklendi.");
